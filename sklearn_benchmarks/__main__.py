5c174cac1198b249ed1c7d925237e339768fac2c
mbatoul <batoulmathis@gmail.com>
2021-03-26 15:41:33 +0100

    Use Black Formatter for Python file

--- sklearn_benchmarks/__main__.py
+++ sklearn_benchmarks/__main__.py
@@ -4,29 +4,34 @@ import pandas as pd
 from sklearn_benchmarks.core import Algorithm, BenchmarkRunner
 from sklearn_benchmarks.utils import plot
 
-SUPPORTED_ALGOS = ['NearestNeighbors', 'LogisticRegression', 'Ridge']
+SUPPORTED_ALGOS = ["KNeighborsClassifier", "LogisticRegression", "Ridge"]
+
 
 def main():
-  with open('sklearn_benchmarks/config.yaml', 'r') as file:
-    config = yaml.full_load(file)
-  
-  algorithms = [Algorithm(**params) for params in config['algorithms']]
-  datasets = config['datasets']
+    with open("sklearn_benchmarks/config.yaml", "r") as file:
+        config = yaml.full_load(file)
 
-  for algorithm in algorithms:
-    if algorithm.name not in SUPPORTED_ALGOS:
-      continue
+    algorithms = [Algorithm(**params) for params in config["algorithms"]]
+    datasets = config["datasets"]
 
-    benchmark_runner = BenchmarkRunner(**datasets[algorithm.name])
-    results = pd.DataFrame.from_dict(benchmark_runner.run(algorithm))
-    results.to_csv(f'sklearn_benchmarks/benchmarks/daal4py/{algorithm.name}.csv', mode='w+', index=False)
-  
-  files = os.listdir('sklearn_benchmarks/benchmarks/daal4py')
-  for file in files:
-    file = 'sklearn_benchmarks/benchmarks/daal4py/' + file
-    bench_resuls = pd.read_csv(file)
-    plot(bench_resuls)
+    for algorithm in algorithms:
+        if algorithm.name not in SUPPORTED_ALGOS:
+            continue
+
+        benchmark_runner = BenchmarkRunner(**datasets[algorithm.name])
+        results = pd.DataFrame.from_dict(benchmark_runner.run(algorithm))
+        results.to_csv(
+            f"sklearn_benchmarks/benchmarks/daal4py/{algorithm.name}.csv",
+            mode="w+",
+            index=False,
+        )
+
+    files = os.listdir("sklearn_benchmarks/benchmarks/daal4py")
+    for file in files:
+        file = "sklearn_benchmarks/benchmarks/daal4py/" + file
+        bench_resuls = pd.read_csv(file)
+        plot(bench_resuls)
 
 
-if __name__ == '__main__':
-  main()
+if __name__ == "__main__":
+    main()
